c4b91da9ed7c76f74746fb9f0f24fb86984ea3bd
Moisés Souto <moises.souto@gmail.com>
2016-04-11 20:17:48 -0300

    Utilizando o método split para contar o tamanho do texto separado em cada posição de vetor

--- 01_utilizando_objetos/aula01.rb
+++ 01_utilizando_objetos/aula01.rb
@@ -8,3 +8,16 @@ puts frase.empty?
 frase = String.new('Banana')
 puts frase
 puts frase.size
+
+#utilizando o método split
+frase = 'Fulano Beltrano Cicrano'
+puts frase
+puts frase.size
+#utilizando o método split, para separ a string por espaços em branco
+vetor = frase.split()
+puts vetor[0]
+puts vetor[0].size
+puts vetor[1]
+puts vetor[1].size
+puts vetor[2]
+puts vetor[2].size
