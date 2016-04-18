6f5da0f1c8bfec25666d0f7aa589cf5a4d946310
Moisés Souto <moises.souto@gmail.com>
2016-04-18 19:54:15 -0300

    Adicionando exemplos de manipulação com a classe time, utilizando atributos de datas

---
+++ 01_utilizando_objetos/aula02c.rb
@@ -0,0 +1,16 @@
+t = Time.new(2016,04,18)
+puts t
+#apenas o ano
+puts t.year
+#apenas o mês
+puts t.mon
+#apenas o dia
+puts t.day
+#apenas a hora
+puts t.hour
+#apenas minuto
+puts t.min
+#dia da semana (0 para domingo) ->6
+puts t.wday
+#dia do ano
+puts t.yday
