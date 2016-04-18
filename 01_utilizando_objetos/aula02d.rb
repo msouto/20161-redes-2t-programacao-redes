2caf43d758db5d875d0cd9cbb68957e646713ce7
Moisés Souto <moises.souto@gmail.com>
2016-04-18 20:03:48 -0300

    Exemplos de manipulação com strftime

--- 01_utilizando_objetos/aula02d.rb
+++ 01_utilizando_objetos/aula02d.rb
@@ -1,4 +1,9 @@
 t = Time.new(2016,04,18,19,55,30)
 #manipular com diretivas de formatação
 #de data
+#18/04/2016
 puts t.strftime('%d/%m/%Y')
+#18-04-2016 19:55
+puts t.strftime('%d-%m-%Y %H:%M')
+#18 de abril de 2016
+puts t.strftime('%d de %B de %Y')
