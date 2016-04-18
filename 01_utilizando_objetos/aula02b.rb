148383265b640ca1598c401e7ffcfd0fc8f80b24
Moisés Souto <moises.souto@gmail.com>
2016-04-18 19:46:23 -0300

    Mais exemplos de manipulação de datas

---
+++ 01_utilizando_objetos/aula02b.rb
@@ -0,0 +1,15 @@
+t1 = Time.now
+puts t1
+t2 = t1 + 3600
+puts t2
+#comparações entre Time
+
+#retorna a diferença da comparação
+puts t1 <=> t2 
+puts t2 <=> t1 
+puts t1 <=> t1
+
+#comparação de igualdade de datas
+puts t2==t2
+puts t1>=t2
+puts t1<t2
