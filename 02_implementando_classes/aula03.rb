2e44f8c098b8a9f454a6986f813e91bf2f25d297
Moisés Souto <moises.souto@gmail.com>
2016-04-20 20:11:59 -0300

    Modificando o construtor para inicliazar por parâmetro

--- 02_implementando_classes/aula03.rb
+++ 02_implementando_classes/aula03.rb
@@ -2,6 +2,11 @@ class Fracao
   attr_accessor :numerador
   attr_reader :denominador
 
+def initialize(num,den)
+	@numerador=num
+	@denominador=den
+end
+
   def denominador=(den)
     if den.to_i != 0
       @denominador = den
@@ -15,13 +20,10 @@ class Fracao
 end
 
 #codigo de teste
-f = Fracao.new
 puts "Alterando os valor da fração para 3/5"
-f.numerador = 3
-f.denominador = 5
+f = Fracao.new(3,5)
 puts f.to_s
 
 puts "Alterando os valor da fração para 8/7"
-f.numerador = 8
-f.denominador = 7
-puts f.to_s
+f1 = Fracao.new(8,7)
+puts f1.to_s
