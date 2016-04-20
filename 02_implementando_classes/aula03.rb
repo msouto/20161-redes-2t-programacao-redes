f500de08c3115be5c3b03bd5b0b66f3fe56c0960
Moisés Souto <moises.souto@gmail.com>
2016-04-20 20:00:48 -0300

    Sobrecarregando o método to_s para exibição dos dados da fração

--- 02_implementando_classes/aula03.rb
+++ 02_implementando_classes/aula03.rb
@@ -8,10 +8,20 @@ class Fracao
     end
   end
 
+  def to_s
+	puts @numerador.to_s + '/' + @denominador.to_s
+  end
+
 end
 
 #codigo de teste
 f = Fracao.new
+puts "Alterando os valor da fração para 3/5"
 f.numerador = 3
 f.denominador = 5
-puts f.numerador.to_s + '/' + f.denominador.to_s
+puts f.to_s
+
+puts "Alterando os valor da fração para 8/7"
+f.numerador = 8
+f.denominador = 7
+puts f.to_s
