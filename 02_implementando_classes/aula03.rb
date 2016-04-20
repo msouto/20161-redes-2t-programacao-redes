f87a4746c39fc0e1c32d956c29a031264c206e05
Moisés Souto <moises.souto@gmail.com>
2016-04-20 19:51:21 -0300

    Adicionando método para atribuição do valor do denominador

--- 02_implementando_classes/aula03.rb
+++ 02_implementando_classes/aula03.rb
@@ -1,8 +1,17 @@
 class Fracao
   attr_accessor :numerador
   attr_reader :denominador
+
+  def denominador=(den)
+    if den.to_i != 0
+      @denominador = den
+    end
+  end
+
 end
 
 #codigo de teste
 f = Fracao.new
+f.numerador = 3
+f.denominador = 5
 puts f.numerador.to_s + '/' + f.denominador.to_s
