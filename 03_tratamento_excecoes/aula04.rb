b7ffc347ddd11fb8a570a3bf1527d96324f96acf
Moisés Souto <moises.souto@gmail.com>
2016-04-27 19:44:46 -0300

    Exibindo a mensagem com a exceção gerada

--- 03_tratamento_excecoes/aula04.rb
+++ 03_tratamento_excecoes/aula04.rb
@@ -2,8 +2,10 @@ begin
 	#código a ser testado para exceções
 	x=0
 	y=5/x
-rescue Exception
+rescue Exception => ex
 	#código de tratamento caso ocorra a exceção
 	y=5
+	#exibir a informação do tratamento da exceção
+	puts ex
 end	
 puts y
