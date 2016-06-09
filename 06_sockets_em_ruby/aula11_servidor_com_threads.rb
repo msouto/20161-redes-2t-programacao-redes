031ed902b5b8610bf75592a87342ee0b645c658d
Moisés Souto <moises.souto@gmail.com>
2016-06-08 22:05:40 -0300

    Adicionando tratamento de múltiplas conexões

--- 06_sockets_em_ruby/aula11_servidor_com_threads.rb
+++ 06_sockets_em_ruby/aula11_servidor_com_threads.rb
@@ -1,14 +1,21 @@
 require 'socket'
-
 serverSocket = TCPServer.new(2000)
 
 loop {
-	Thread.start(serverSocket.accept) do |clientSocket| 
+	t = Thread.start(serverSocket.accept) do |clientSocket| 
+		inicio = Time.now
+		sock_domain, remote_port, remote_hostname, remote_ip = clientSocket.peeraddr
+		puts "Conexão de #{remote_ip} na porta #{remote_port}:"
+		puts t
 		msgcliente = clientSocket.gets
 		#simula uma operação demorada
 		#Como E/S disco
 		sleep(2)
 		clientSocket.puts('ok')
+		puts "\n"
+		fim = Time.now
+		#calcula e imprimir o tempo de execução
+		puts "#{fim - inicio} s"
 		clientSocket.close
 	end
  }
