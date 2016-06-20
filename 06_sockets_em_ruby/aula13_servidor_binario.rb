1050a166bac63604a042354ad78b32fb9475cccc
Moisés Souto <moises.souto@gmail.com>
2016-06-20 19:54:59 -0300

    Adicionando servidor de transferência binaria

---
+++ 06_sockets_em_ruby/aula13_servidor_binario.rb
@@ -0,0 +1,15 @@
+require 'socket'
+
+serverSocket = TCPServer.new(2000)
+clientSocket = serverSocket.accept
+
+begin
+	nome_arquivo = clientSocket.gets.chomp
+	tamanho_arquivo = clientSocket.gets.chomp.to_i
+	bytes_arquivo = clientSocket.read(tamanho_arquivo)	
+	arquivo = File.new("copia #{nome_arquivo}", "wb")
+	arquivo.syswrite(bytes_arquivo)
+ensure
+	serverSocket.close
+	arquivo.close
+end
