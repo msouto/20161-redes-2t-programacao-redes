3c87473eeac921eb0a937d895677f144a256099d
Moisés Souto <moises.souto@gmail.com>
2016-07-13 20:09:06 -0300

    Implementando classe e métodos de tratamento do usuário. Além disso, foi implementado um cliente mínimo de testes.

--- 08_web_services/aula19.rb
+++ 08_web_services/aula19.rb
@@ -3,3 +3,6 @@ require "net/http"
 http = Net::HTTP.new("localhost", 4567)
 
 #cadastrar usuário
+request = Net::HTTP::PUT.new("/usuario")
+request.body = "<?xml?><usuario nome='Ana' />"
+response = http.request(request)
--- 08_web_services/aula19_servidor.rb
+++ 08_web_services/aula19_servidor.rb
@@ -1,9 +1,27 @@
+#implementação do serviço REST.
+#Utiliza a biblioteca Sinatra, a qual deve ser instalada
+#gem install sinatra
 require "sinatra"
 require "rexml/document"
 include REXML
 
+class Usuario
+	attr_accessor :nome, :links
+
+	def initialize(nome)
+		@nome = nome
+		@links = Hash.new
+		@contador = 1
+	end
+
+end
+#inicializar variaveis globais
+$contador = 1
+$usuarios = Hash.new
+
+
 get '/' do
-	'Hello World!'
+	'Seja bem-vindo ao sistema de bookmark de REDES-2N'
 end
 
 get '/usuario/:id' do 
@@ -11,5 +29,27 @@ get '/usuario/:id' do
 	usuario = $usuarios[id_usuario.to_i]
 	if usuario.nil?
 		status 404
+	else
+		status 200
+		content_type 'text/xml'
+		"<?xml version='1.0' ?><usuario nome='#{usuario.nome}'/>"
 	end
 end
+
+put '/usuario' do
+	#construindo a requisicao de cadastro de usuario
+	doc = Document.new(request.body.string)
+	nome = doc.elements[1].attributes["nome"]
+	if nome.nil?
+		status 404
+	else
+		usuario = Usuario.new(nome)
+		$usuarios[$contador] = usuario
+		puts ">>>> Usuario criado com o ID: #{$contador}"
+		$contador += 1
+		status 200
+	end
+end
+
+
+
