066e75b452ca3d823c8f0b59da5c5860735cbdb8
Moisés Souto <moises.souto@gmail.com>
2016-07-18 20:25:51 -0300

    Exemplos de manipulacao de arquuvos XML

--- 09_xml/aula20.rb
+++ 09_xml/aula20.rb
@@ -9,3 +9,44 @@ puts "** Tags dos paises vencedores"
 doc.elements.each("campeoes/selecao/pais") do |p|
 	puts p
 end
+
+#listando os nomes dos países vencedores de copas
+puts "** Nomes dos paises vencedores"
+doc.elements.each("campeoes/selecao/pais") do |p|
+	puts p.text
+end
+
+#listando as copas realizadas
+puts "** Copas realizadas"
+doc.elements.each("campeoes/selecao/copas_do_mundo/copa") do |c|
+	puts "#{c.attributes['ano']} - #{c.attributes['local']}"
+end
+
+#listando os paises europeus vencedores de copas
+puts "** Paises europeus vencedores de copas"
+doc.elements.each("campeoes/selecao[continente='Europa']/pais") do |p|
+	puts p.text
+end
+
+#listando os anos das copas deste século
+puts "** Anos das copas do século 21"
+doc.elements.each("campeoes/selecao/copas_do_mundo/copa[@ano>=2000]") do |p|
+	puts p.attributes['ano']
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
