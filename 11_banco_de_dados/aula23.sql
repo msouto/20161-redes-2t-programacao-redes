de831aba454b68da41f70e233ccbde607b10b170
Moisés Souto <moises.souto@gmail.com>
2016-08-08 19:39:58 -0300

    Adicionando inserção de dados SQL, exercicio 2

---
+++ 11_banco_de_dados/aula23.sql
@@ -0,0 +1,9 @@
+--1) inserir registros exercicio 2
+insert into produtos(codigo, descricao, unidade, valor_unitario) values (25, 'Queijo', 'Kg', 0.97);
+insert into produtos(codigo, descricao, unidade, valor_unitario) values (77, 'Papel', 'm', 1.05);
+insert into produtos (codigo, descricao, unidade, valor_unitario) values (31, 'Chocolate', 'Und', 0.87);
+insert into produtos (codigo, descricao, unidade, valor_unitario) values (78, 'Vinho', 'L', 8.0);
+insert into produtos (codigo, descricao, unidade, valor_unitario) values (13, 'Ouro', 'g', 26.18);
+insert into produtos (codigo, descricao, unidade, valor_unitario) values (45, 'Madeira', 'm', 12.5);
+insert into produtos (codigo, descricao, unidade, valor_unitario) values (53, 'Açucar', 'SC', 1.2);
+insert into produtos (codigo, descricao, unidade, valor_unitario) values (22, 'Cano', 'm', 0.65);
